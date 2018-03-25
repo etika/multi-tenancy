637302187b878f55cc1481f421e0382e8cd7f34b
william <willshinji@gmail.com>
2018-03-25 00:04:12 -0300

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+require 'apartment/elevators/subdomain'
+
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MultiTenancy
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Multi-tenancy
+    config.middleware.use Apartment::Elevators::Subdomain
+
+  end
+end
